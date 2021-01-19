15c8dd56ba8d3274540e09063993da7ffc03248c
dwiknrd <dwiknrd@gmail.com>
2021-01-19 13:53:41 +0700

    adding last project

---
+++ boilerplate-sea-level-predictor-1/sea_level_predictor.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
+
+def draw_plot():
+    # Read data from file
+    sea_lvl = pd.read_csv('epa-sea-level.csv')
+
+
+    # Create scatter plot
+    slfig = sea_lvl.plot(
+        kind='scatter', x='Year', y='CSIRO Adjusted Sea Level', figsize=(6, 6))
+
+    # Create first line of best fit
+    year_limit = 2050
+    slope1, intercept1, r_value1, p_value1, std_err1 = linregress(
+        sea_lvl['Year'], sea_lvl['CSIRO Adjusted Sea Level'])
+
+    df_predict1 = sea_lvl[['Year', 'CSIRO Adjusted Sea Level']].copy()
+
+    for i in range(df_predict1['Year'].max() + 1, year_limit):
+        df_predict1 = df_predict1.append(
+            {
+                'Year': i,
+                'CSIRO Adjusted Sea Level': intercept1 + slope1 * i
+            },
+            ignore_index=True)
+
+    # Create second line of best fit
+    df_predict2 = sea_lvl[(sea_lvl['Year'] >= 2000)
+                          & (sea_lvl['Year'] <= sea_lvl['Year'].max())]
+
+    slope2, intercept2, r_value2, p_value2, std_err2 = linregress(
+        df_predict2['Year'], df_predict2['CSIRO Adjusted Sea Level'])
+
+    for i in range(df_predict2['Year'].max() + 1, year_limit):
+        df_predict2 = df_predict2.append(
+            {
+                'Year': i,
+                'CSIRO Adjusted Sea Level': intercept2 + slope2 * i
+            },
+            ignore_index=True)
+
+    plt.plot(
+        df_predict2['Year'],
+        intercept2 + slope2 * df_predict2['Year'],
+        'tab:green',
+        label='Prediction 2000-2050')
+    plt.legend()
+
+    # Add labels and title
+    plt.xlabel("Year")
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
+    
+    # Save plot and return data for testing (DO NOT MODIFY)
+    plt.savefig('sea_level_plot.png')
+    return plt.gca()
